2569508877b79bef6bb820044cea8c7e103b19ac
Phan Sĩ Thành <thanhphanscpy2005@gmail.com>
2026-07-13 14:53:47 +0700

    first commit

---
+++ src/test/java/com/intern/booking_event/BookingEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.booking_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
