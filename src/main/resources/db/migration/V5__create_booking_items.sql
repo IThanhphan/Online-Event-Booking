962084ce08b77306bc36999c17dde86f5dfb0db3
Phan Sĩ Thành <thanhphanscpy2005@gmail.com>
2026-07-14 10:21:49 +0700

    Revert "Merge pull request #3 from IThanhphan/tu/dev"
    This reverts commit 15d33aa7394d1e676b5f433086e0f29e2a103462, reversing
    changes made to 014bf1bdd826c1d9e4a6bf642bb8efa111e2b0cb.

--- src/main/resources/db/migration/V5__create_booking_items.sql
+++ src/main/resources/db/migration/V5__create_booking_items.sql
@@ -5,11 +5,5 @@ CREATE TABLE booking_items (
     quantity INT NOT NULL,
     unit_price DECIMAL(19, 2) NOT NULL,
     CONSTRAINT fk_booking_item_booking FOREIGN KEY (booking_id) REFERENCES bookings(id) ON DELETE CASCADE,
-    CONSTRAINT fk_booking_item_ticket_type FOREIGN KEY (ticket_type_id) REFERENCES ticket_types(id),
-
-    -- Thêm UNIQUE constraint và CHECK constraint cho quantity
-    --  Đảm bảo một loại vé không bị lặp lại dòng trong cùng một đơn hàng
-    CONSTRAINT uq_booking_ticket_type UNIQUE (booking_id, ticket_type_id),
-    --Khách hàng phải chọn ít nhất 1 vé khi đặt mua
-    CONSTRAINT chk_booking_item_quantity CHECK (quantity > 0),
+    CONSTRAINT fk_booking_item_ticket_type FOREIGN KEY (ticket_type_id) REFERENCES ticket_types(id)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
