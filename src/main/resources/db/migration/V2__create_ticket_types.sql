bf2e84eb16ccf581bb4f9070deece3b6f5e45c67
TuNgoc882005 <tu.nn.65cntt@ntu.edu.vn>
2026-07-14 09:34:12 +0700

    add Constaint for db

--- src/main/resources/db/migration/V2__create_ticket_types.sql
+++ src/main/resources/db/migration/V2__create_ticket_types.sql
@@ -5,5 +5,9 @@ CREATE TABLE ticket_types (
     price DECIMAL(19, 2) NOT NULL,
     total_quantity INT NOT NULL,
     sold_quantity INT NOT NULL DEFAULT 0,
-    CONSTRAINT fk_ticket_type_event FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
+    CONSTRAINT fk_ticket_type_event FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE,
+
+    -- Thêm CHECK để bảo vệ Kho vé
+    CONSTRAINT chk_ticket_price CHECK (price >= 0),--Ngăn chặn lỗi cấu hình giá vé bị âm
+    CONSTRAINT chk_sold_quantity CHECK (sold_quantity <= total_quantity),--Số lượng vé bán ra không được vượt quá kho
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
