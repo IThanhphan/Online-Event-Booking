f3d0fdb6515577d6f75aa1283c7513c6bc09a79b
TuNgoc882005 <tu.nn.65cntt@ntu.edu.vn>
2026-08-12 13:38:56 +0700

    update event api

--- src/main/java/com/intern/booking_event/controller/EventController.java
+++ src/main/java/com/intern/booking_event/controller/EventController.java
@@ -1,15 +1,14 @@
 package com.intern.booking_event.controller;
 
+import com.intern.booking_event.model.dto.request.AiRequest;
 import com.intern.booking_event.model.dto.request.EventRequest;
+import com.intern.booking_event.model.dto.response.AiResponse;
 import com.intern.booking_event.model.dto.response.ApiResponse;
 import com.intern.booking_event.model.dto.response.EventResponse;
+import com.intern.booking_event.service.AIService;
 import com.intern.booking_event.service.EventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
@@ -20,6 +19,7 @@ import java.time.Instant;
 public class EventController {
 
     private final EventService eventService;
+    private final AIService aiService;
 
     @PostMapping
     public ApiResponse<EventResponse> createEvent(@RequestBody EventRequest request) {
@@ -48,9 +48,18 @@ public class EventController {
     }
 
     @GetMapping("/{id}")
-    private ApiResponse<EventResponse> getEventById(@PathVariable Long id) {
+    public ApiResponse<EventResponse> getEventById(@PathVariable Long id) {
         return ApiResponse.<EventResponse>builder()
                 .result(eventService.getEventById(id))
+                .build();
+    }
+
+    //
+    @PostMapping("/{id}/ask")
+    public ApiResponse<AiResponse> askEventIdWithAi(@PathVariable Long id, @RequestBody AiRequest request) {
+        AiResponse response = aiService.askEventIdWithAi(id,request);
+        return ApiResponse.<AiResponse>builder()
+                .result(response)
                 .build();
     }
 }
--- src/main/java/com/intern/booking_event/repository/EventRepository.java
+++ src/main/java/com/intern/booking_event/repository/EventRepository.java
@@ -3,6 +3,7 @@ package com.intern.booking_event.repository;
 import com.intern.booking_event.model.entity.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -13,7 +14,8 @@ import java.time.Instant;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
-    @Query("SELECT DISTINCT e FROM Event e LEFT JOIN FETCH e.ticketTypes "+
+    @EntityGraph(attributePaths = {"ticketTypes"})
+    @Query("SELECT DISTINCT e FROM Event e "+
         "WHERE(:title IS NULL OR :title = '' OR LOWER(e.title) LIKE LOWER(CONCAT('%', :title, '%'))) "+
             "AND (:category IS NULL OR :category = '' OR e.category = :category) "+
             "AND (:venue IS NULL OR :venue = '' OR LOWER(e.venue) LIKE LOWER(CONCAT('%', :venue, '%'))) "+
@@ -28,5 +30,4 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             @Param("endDate") Instant endDate,
             Pageable pageable
     );
-
 }
